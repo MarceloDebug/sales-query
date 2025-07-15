526107f680517f8ed70ac8b2993b283c32addcdd
MarceloDebug <marcelinho.soares300@gmail.com>
2025-07-15 00:29:46 -0300

    First Commit

---
+++ src/main/java/com/marceloDebug/sales_query/SalesQueryApplication.java
@@ -0,0 +1,13 @@
+package com.marceloDebug.sales_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	SalesQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marceloDebug/sales_query/SalesQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloDebug.sales_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
