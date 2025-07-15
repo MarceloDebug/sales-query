6fbe864264695815af0aab661bc96f2c75b19aea
MarceloDebug <marcelinho.soares300@gmail.com>
2025-07-15 03:15:44 -0300

    refactor(repository): remove unused SellerRepository

--- src/main/java/com/marceloDebug/sales_query/repositories/SellerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marceloDebug.sales_query.repositories;
-
-import com.marceloDebug.sales_query.entities.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-}
